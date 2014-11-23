40f04dd1c86e41264ae7069209c01634f4c89162
diagnosa <d.fenomena@live.com>
2014-11-23 09:15:22 +0700

    renamed

--- Client/src/client/resources/ClientGUI.java
+++ Client/src/client/resources/ClientGUI.java
@@ -22,12 +22,12 @@ import javax.swing.filechooser.FileFilter;
  * @author Diagnosa
  */
 
-public class CilentGUI extends javax.swing.JFrame {
+public class ClientGUI extends javax.swing.JFrame {
 
     /**
      * Creates new form CilentGUI
      */
-    public CilentGUI() {
+    public ClientGUI() {
         initComponents();
     }
 
@@ -149,7 +149,7 @@ public class CilentGUI extends javax.swing.JFrame {
         // TODO add your handling code here:
         Socket sock;
         try {
-            sock = new Socket("127.0.0.1", 6060);
+            sock = new Socket("127.0.0.1", 13267);
             byte[] mybytearray = new byte[1024];
             InputStream is = sock.getInputStream();
             FileOutputStream fos = new FileOutputStream(file.getAbsolutePath());
@@ -159,7 +159,7 @@ public class CilentGUI extends javax.swing.JFrame {
             bos.close();
             sock.close();
         } catch (IOException ex) {
-            Logger.getLogger(CilentGUI.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(ClientGUI.class.getName()).log(Level.SEVERE, null, ex);
         }
     
     }//GEN-LAST:event_UploadActionPerformed
@@ -181,20 +181,21 @@ public class CilentGUI extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(CilentGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(ClientGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(CilentGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(ClientGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(CilentGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(ClientGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(CilentGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(ClientGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new CilentGUI().setVisible(true);
+                new ClientGUI().setVisible(true);
             }
         });
     }
